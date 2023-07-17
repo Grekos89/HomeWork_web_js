0188e090b43a10a2b21b9c864b216e27cafca8dd
Евгений <Grekos_89@mail.ru>
2023-07-17 13:51:05 +0300

    домашняя работа

---
+++ homework.js
@@ -0,0 +1,12 @@
+let age = +(prompt("How old are you"));
+alert("hi, you are is " + age + " years old");
+switch (age) {
+    case 35:
+        alert("You are older than me");
+        break;
+    case 33:
+        alert("You are yonger than me");
+        break;
+    default:
+        alert("Your age is far from my")
+}
